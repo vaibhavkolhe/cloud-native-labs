d38abd1d5d5fb683a5d015f96ba165ebcaa80c59
vaibhavkolhe <vaibhav.kolhe@accenture.com>
2018-11-09 12:32:02 -0500

    Update GatewayVerticle.java

--- gateway-vertx/src/main/java/com/redhat/cloudnative/gateway/GatewayVerticle.java
+++ gateway-vertx/src/main/java/com/redhat/cloudnative/gateway/GatewayVerticle.java
@@ -31,7 +31,7 @@ public class GatewayVerticle extends AbstractVerticle {
         router.route().handler(CorsHandler.create("*").allowedMethod(HttpMethod.GET));
         router.get("/health").handler(ctx -> ctx.response().end(new JsonObject().put("status", "UP").toString()));
         // Update the line below
-        router.get("/hello").handler(ctx -> ctx.response().end(new JsonObject().put("message", new Timestamp(new Date().getTime()) + " Hello, World!-Update").toString() + "\n"));
+        router.get("/hello").handler(ctx -> ctx.response().end(new JsonObject().put("message", new Timestamp(new Date().getTime()) + " Hello, World! - Update").toString() + "\n"));
         router.get("/api/products").handler(this::products);
 
         ServiceDiscovery.create(vertx, discovery -> {
